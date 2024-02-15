884eac0ac6b7d787b96c95e1aafd8da8437a4af7
Saksham Walia <waliasaksham02@gmail.com>
2024-02-15 18:13:29 +0530

    third commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ## MLflow experiments
+
+MLFLOW_TRACKING_URI=https://dagshub.com/sakshamwalia/mlflowexperiments.mlflow \
+MLFLOW_TRACKING_USERNAME=sakshamwalia \
+MLFLOW_TRACKING_PASSWORD=549c5cc4ae00fd52e55b92e68cec5dc45a19bc65 \
+python script.py
--- app.py
+++ app.py
@@ -77,6 +77,10 @@ if __name__ == "__main__":
         predictions = lr.predict(train_x)
         signature = infer_signature(train_x, predictions)
 
+        ## FOR REMOTE SERVER (DAGSHUB)
+        remote_server_uri = "https://dagshub.com/sakshamwalia/mlflowexperiments.mlflow"
+        mlflow.set_tracking_uri = remote_server_uri
+        
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
         # Model registry does not work with file store
